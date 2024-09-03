c6dbdb118247f028b84ef48ccc9c1910669001f9
Theekshana Thathsara <theekshanathathsara.2001@gmail.com>
2024-09-04 00:46:40 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://thathsara:rm8Rf2KNPWAppkWC@cluster0.x4exe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("Database Successfully Connected "))
+}
+//rm8Rf2KNPWAppkWC
---
+++ backend/server.js
@@ -0,0 +1,83 @@
+/*import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/UserRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+ 
+ 
+
+
+
+//app config
+const app = express()
+//const port = 4000
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+ 
+ 
+ 
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Started on http://localhost:${PORT}`)
+})
+
+ */
+
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/UserRoute.js";
+import 'dotenv/config';
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
