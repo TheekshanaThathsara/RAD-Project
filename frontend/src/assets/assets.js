2fd89ea9dd7630280123cbd7ef1344ad2601b582
Theekshana Thathsara <theekshanathathsara.2001@gmail.com>
2024-09-08 21:37:14 +0530

    frontend

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,14 +2,12 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-import menu_1 from './menu_1.png'
-import menu_2 from './menu_2.png'
-import menu_3 from './menu_3.png'
-import menu_4 from './menu_4.png'
-import menu_5 from './menu_5.png'
-import menu_6 from './menu_6.png'
-import menu_7 from './menu_7.png'
-import menu_8 from './menu_8.png'
+import list1 from './list1.png'
+import list2 from './list2.png'
+import list3 from './list3.png'
+import list4 from './list4.png'
+import list5 from './list5.png'
+
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -84,36 +82,24 @@ export const assets = {
 
 export const menu_list = [
     {
-        menu_name: "Salad",
-        menu_image: menu_1
+        menu_name: "Fiction",
+        menu_image: list1
     },
     {
-        menu_name: "Rolls",
-        menu_image: menu_2
+        menu_name: "Fantasy",
+        menu_image: list2
     },
     {
-        menu_name: "Deserts",
-        menu_image: menu_3
+        menu_name: "Mystry & Thriller",
+        menu_image: list3
     },
     {
-        menu_name: "Sandwich",
-        menu_image: menu_4
+        menu_name: "Non Fiction",
+        menu_image: list4
     },
     {
-        menu_name: "Cake",
-        menu_image: menu_5
-    },
-    {
-        menu_name: "Pure Veg",
-        menu_image: menu_6
-    },
-    {
-        menu_name: "Pasta",
-        menu_image: menu_7
-    },
-    {
-        menu_name: "Noodles",
-        menu_image: menu_8
+        menu_name: "Science Fiction",
+        menu_image: list5
     }]
 
 export const food_list = [
